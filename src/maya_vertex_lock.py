5bd5966e17c6191ec7dfc40c425df3656fc3ba67
NinaMina2737 <87418579+NinaMina2737@users.noreply.github.com>
2024-02-04 19:31:56 +0900

    Update maya_vertex_lock.py

--- src/maya_vertex_lock.py
+++ src/maya_vertex_lock.py
@@ -13,6 +13,7 @@ def toggle_lock_vertex():
         cmds.setAttr(selected_vertex + ".px", lock=not (cmds.getAttr(selected_vertex + ".px", lock=True)))
         cmds.setAttr(selected_vertex + ".py", lock=not (cmds.getAttr(selected_vertex + ".py", lock=True)))
         cmds.setAttr(selected_vertex + ".pz", lock=not (cmds.getAttr(selected_vertex + ".pz", lock=True)))
+    print("Vertex Lock Toggled({}): {}".format(len(list_selected_vertex), list_selected_vertex))
     cmds.undoInfo(closeChunk=True)
 
 
@@ -23,6 +24,21 @@ def lock_vertex_lock():
         cmds.setAttr(selected_vertex + ".px", lock=True)
         cmds.setAttr(selected_vertex + ".py", lock=True)
         cmds.setAttr(selected_vertex + ".pz", lock=True)
+    print("Vertex Locked({}): {}".format(len(list_selected_vertex), list_selected_vertex))
+    cmds.undoInfo(closeChunk=True)
+
+
+def lock_all_vertex_lock():
+    list_all_object = cmds.ls(dag=True, long=True, type="mesh")
+    list_all_vertex = []
+    for obj in list_all_object:
+        list_all_vertex.extend(cmds.ls(obj + ".vtx[*]", flatten=True))
+    cmds.undoInfo(openChunk=True)
+    for vertex in list_all_vertex:
+        cmds.setAttr(vertex + ".px", lock=True)
+        cmds.setAttr(vertex + ".py", lock=True)
+        cmds.setAttr(vertex + ".pz", lock=True)
+    print("All Vertex Locked({}): {}".format(len(list_all_vertex), list_all_vertex))
     cmds.undoInfo(closeChunk=True)
 
 
@@ -33,5 +49,20 @@ def unlock_vertex_lock():
         cmds.setAttr(selVtx + ".px", lock=False)
         cmds.setAttr(selVtx + ".py", lock=False)
         cmds.setAttr(selVtx + ".pz", lock=False)
+    print("Vertex Unlocked({}): {}".format(len(list_selected_vertex), list_selected_vertex))
+    cmds.undoInfo(closeChunk=True)
+
+
+def unlock_all_vertex_lock():
+    list_all_object = cmds.ls(dag=True, long=True, type="mesh")
+    list_all_vertex = []
+    for obj in list_all_object:
+        list_all_vertex.extend(cmds.ls(obj + ".vtx[*]", flatten=True))
+    cmds.undoInfo(openChunk=True)
+    for vertex in list_all_vertex:
+        cmds.setAttr(vertex + ".px", lock=False)
+        cmds.setAttr(vertex + ".py", lock=False)
+        cmds.setAttr(vertex + ".pz", lock=False)
+    print("All Vertex Unlocked({}): {}".format(len(list_all_vertex), list_all_vertex))
     cmds.undoInfo(closeChunk=True)
 
